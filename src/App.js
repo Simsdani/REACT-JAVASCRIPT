275f0c00ba2da98e7ca7e731e8f58be8a33a879a
Simsdani <135972763+Simsdani@users.noreply.github.com>
2025-03-25 01:01:21 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import PostCard from './PostCard';
+
+
+function App() {
+    return (
+        <div style={{ padding: "45px", display: "flex", justifyContent: "center" }}>
+          <PostCard/>
+        </div>
+      );
+    
+}
+
+export default App;
